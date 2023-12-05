128870b97e5f8b7751685fdb7f861adf9a900c50
Super Ice Boy <romchik4353@gmail.com>
2023-12-05 21:41:58 +0500

    v.0.0.4

--- super ice boy/Assets/Scripts/Enemy.cs
+++ super ice boy/Assets/Scripts/Enemy.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    public float speed;
+
     public PlayerMovement player;
     public Termometr Termometr;
     private void Awake()
@@ -34,16 +40,39 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
         gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+            {
+            currentPoint = pointB.transform;
+        }
 
     }
     public void Setactive()
     {
         gameObject.SetActive(false);
     }
+
+
+
 }
